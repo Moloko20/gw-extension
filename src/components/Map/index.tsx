4ad2784bdf08527e5164cf1c4bfb60767ea0d743
Moloko20 <alexvfrontend@gmail.com>
2022-05-16 20:07:25 +0300

    feat(map): get right data

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,13 +1,48 @@
 import React from 'react'
-
 import { MapContainer, Marker, Popup, CircleMarker } from 'react-leaflet'
+
+// import { toSec } from '@foxglove/rostime'
 
 import { Layers } from 'components/Layers'
 
+import { MapPanelMessage } from 'types/MapPanelMessage'
+
 import './index.css'
 
-export function Map(): JSX.Element {
-    console.log('none', Marker, Popup)
+type MapProps = {
+    messages: MapPanelMessage[]
+}
+
+export function Map({ messages }: MapProps): JSX.Element {
+    console.log('none', Marker, Popup, messages)
+
+    // React.useEffect(() => {
+    //     // if (!currentMap || previewTime == undefined) {
+    //     //     return
+    //     // }
+
+    //     // get the point occuring most recently before preview time but not after preview time
+    //     const prevNavMessages = messages.filter(message => toSec(message.receiveTime) < previewTime)
+    //     const event = minBy(prevNavMessages, message => previewTime - toSec(message.receiveTime))
+    //     if (!event) {
+    //         return
+    //     }
+
+    //     const topicLayer = topicLayers.get(event.topic)
+
+    //     const marker = new CircleMarker([event.message.latitude, event.message.longitude], {
+    //         radius: POINT_MARKER_RADIUS,
+    //         color: topicLayer ? darkColor(topicLayer.baseColor) : undefined,
+    //         stroke: false,
+    //         fillOpacity: 1,
+    //         interactive: false,
+    //     })
+
+    //     marker.addTo(currentMap)
+    //     return () => {
+    //         marker.remove()
+    //     }
+    // }, [allNavMessages, currentMap, previewTime, topicLayers])
 
     return (
         <MapContainer
