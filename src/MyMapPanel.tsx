0634fa35209e2b1535dfc34e81d7b2a535c728f8
Moloko20 <alexvfrontend@gmail.com>
2022-05-16 20:11:14 +0300

    feat(map): get right data

--- src/MyMapPanel.tsx
+++ src/MyMapPanel.tsx
@@ -2,8 +2,10 @@ import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 
 import { PanelExtensionContext, RenderState, Topic, MessageEvent } from '@foxglove/studio'
+// import { toSec } from '@foxglove/rostime'
 
-import { Map } from 'components/Map'
+// import { Map } from 'components/Map'
+import { MapPanelMessage } from 'types/MapPanelMessage'
 
 // import L from 'leaflet'
 // import LeafletRetinaIconUrl from 'leaflet/dist/images/marker-icon-2x.png'
@@ -23,8 +25,6 @@ import { Map } from 'components/Map'
 //     shadowSize: [41, 41],
 // })
 
-// console.log(L.Marker.prototype)
-
 //const destDir = path_1.join("/mnt/c/Users/Alex/.foxglove-studio/extensions", dirName);
 
 import './index.sass'
@@ -32,6 +32,10 @@ import './index.sass'
 export function MyMapPanel({ context }: { context: PanelExtensionContext }): JSX.Element {
     const [topics, setTopics] = React.useState<readonly Topic[] | undefined>()
     const [messages, setMessages] = React.useState<readonly MessageEvent<unknown>[] | undefined>()
+
+    const [allMapMessages, setAllMapMessages] = React.useState<MapPanelMessage[]>([])
+
+    const [previewTime, setPreviewTime] = React.useState<number | undefined>()
 
     const [renderDone, setRenderDone] = React.useState<(() => void) | undefined>()
 
@@ -48,39 +52,85 @@ export function MyMapPanel({ context }: { context: PanelExtensionContext }): JSX
 
         // The render handler could be invoked as often as 60hz during playback if fields are changing often.
 
+        context.watch('currentFrame')
+        context.watch('topics')
+        context.watch('allFrames')
+        context.watch('previewTime')
+
         context.onRender = (renderState: RenderState, done) => {
             // render functions receive a _done_ callback. You MUST call this callback to indicate your panel has finished rendering.
             // Your panel will not receive another render callback until _done_ is called from a prior render. If your panel is not done
             // rendering before the next render call, studio shows a notification to the user that your panel is delayed.
             //
             // Set the done callback into a state variable to trigger a re-render.
-            setRenderDone(done)
+
+            setRenderDone(() => done)
+
+            setPreviewTime(renderState.previewTime)
 
             // We may have new topics - since we are also watching for messages in the current frame, topics may not have changed
             // It is up to you to determine the correct action when state has not changed.
-            setTopics(renderState.topics)
 
             // currentFrame has messages on subscribed topics since the last render call
             setMessages(renderState.currentFrame)
+
+            if (renderState.topics) {
+                setTopics(renderState.topics)
+            }
+
+            if (renderState.allFrames) {
+                setAllMapMessages(renderState.allFrames as MapPanelMessage[])
+            }
+
+            // Only update the current frame if we have new messages.
+            // if (renderState.currentFrame && renderState.currentFrame.length > 0) {
+            //     setCurrentMapMessages(renderState.currentFrame as MapPanelMessage[])
+            // }
         }
 
         // After adding a render handler, you must indicate which fields from RenderState will trigger updates.
         // If you do not watch any fields then your panel will never render since the panel context will assume you do not want any updates.
 
         // tell the panel context that we care about any update to the _topic_ field of RenderState
-        context.watch('topics')
 
         // tell the panel context we want messages for the current frame for topics we've subscribed to
         // This corresponds to the _currentFrame_ field of render state.
-        context.watch('currentFrame')
 
         // subscribe to some topics, you could do this within other effects, based on input fields, etc
         // Once you subscribe to topics, currentFrame will contain message events from those topics (assuming there are messages).
-        context.subscribe(['/some/topic'])
+        context.subscribe(['/gps'])
 
         console.table(topics)
         console.table(messages)
-    }, [])
+    }, [context, messages, topics])
+
+    // React.useEffect(() => {
+    //     // if (!currentMap || previewTime == undefined) {
+    //     //     return
+    //     // }
+
+    //     // get the point occuring most recently before preview time but not after preview time
+    //     const prevNavMessages = messages.filter(message => toSec(message.receiveTime) < previewTime)
+    //     const event = minBy(prevNavMessages, message => previewTime - toSec(message.receiveTime))
+    //     if (!event) {
+    //         return
+    //     }
+
+    //     const topicLayer = topicLayers.get(event.topic)
+
+    //     const marker = new CircleMarker([event.message.latitude, event.message.longitude], {
+    //         radius: 2,
+    //         color: '#000',
+    //         stroke: false,
+    //         fillOpacity: 1,
+    //         interactive: false,
+    //     })
+
+    //     marker.addTo(currentMap)
+    //     return () => {
+    //         marker.remove()
+    //     }
+    // }, [allNavMessages, currentMap, previewTime, topicLayers])
 
     // require('./index.sass')
 
@@ -92,19 +142,31 @@ export function MyMapPanel({ context }: { context: PanelExtensionContext }): JSX
     return (
         <>
             <h1>Hi, my name is Alex!</h1>
-            before map
-            <Map />
-            after map{}
-            TOPICS
+            {/* <Map messages={allMapMessages} previewTime={previewTime} /> */}
+            {/* TOPICS 
             <ol>
                 {topics?.map(item => {
                     return <li key={item.name}>ITEM{JSON.stringify(item)}</li>
                 })}
-            </ol>
+            </ol> */}
+            All: {allMapMessages?.length}
+            Not all: {messages?.length}
             MESSAGES
             <ol>
                 {messages?.map(message => {
-                    return <li key={message.topic}>{JSON.stringify(message)}</li>
+                    return <li key={message.topic}>{JSON.stringify(message.message)}</li>
+                })}
+            </ol>
+            ALL_MESSAGES time: {previewTime ?? '0'}
+            <ol>
+                {allMapMessages.map(message => {
+                    return (
+                        <li key={message.topic}>
+                            {JSON.stringify(message.message)}
+
+                            {message.message.latitude}
+                        </li>
+                    )
                 })}
             </ol>
         </>
